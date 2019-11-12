ddd446a8589ae359a8872987f458e180fac089ee
salazhar <canoine.cyril@gmail.com>
2019-11-12 17:16:21 +0100

    first commit

---
+++ inc/data/products.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$catalog = [
+    46 => [
+        'name' => 'Cookies aux noix de pécan',
+        'description' => 'Cuisinés par Penny !',
+    ],
+    36 => [
+        'name' => 'Chips au chocolat',
+        'description' => 'Cuisinés par Bernadette !',
+    ],
+    58 => [
+        'name' => 'Cookies au chocolat',
+        'description' => 'Cuisinés par Bernadette !',
+    ],
+    32 => [
+        'name' => 'Cookies M&N\'s&copy;',
+        'description' => 'Cuisinés par Penny !',
+    ],
+];
